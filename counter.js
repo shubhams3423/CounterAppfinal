f10278c3e1cef9ea1de1a22702b39a106cf71f01
shubhams3423 <shubhams1671h@gmail.com>
2022-11-23 23:23:21 +0530

    counterApplication

---
+++ counter.js
@@ -0,0 +1,17 @@
+
+let zero=0
+function minus(){
+    zero=zero-1
+    // console.log(zero)
+    document.getElementById('zero').innerHTML =zero
+}
+function reset(){
+    zero=0
+    // console.log(zero)
+    document.getElementById('zero').innerHTML =zero
+}
+function plus(){
+    zero=zero+1
+    // console.log(zero)
+    document.getElementById('zero').innerHTML =zero
+}
